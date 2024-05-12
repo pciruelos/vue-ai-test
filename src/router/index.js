7d768df0b536f4c75dd69cec13cc34c979aa3f83
pciruelos <pciruelos@yahoo.com>
2024-05-12 18:17:02 +0200

    init

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import TextView from '../views/TextView.vue'
+
+
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/text',
+      name: 'text',
+      component: TextView
+    }
+  ]
+})
+
+export default router
